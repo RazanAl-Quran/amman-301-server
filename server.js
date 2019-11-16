2d385e92a19e67af49d5d026d74eefc1a1a999b7
RazanAl-Quran <r.alquran@ltuc.com>
2019-11-16 23:00:28 +0200

    first server

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+// to access any files inside the public folder
+server.use(express.static('./public'));
+
+server.get('/test', (request,response) => {
+  response.send('Your test worked');
+});
+
+server.get('/data', (request, response) => {
+  let family = [
+    {name: "John"},
+    {name: "Cathy"},
+    {name: "Zach"},
+    {name: "Allie"}
+  ];
+
+  response.json(family);
+});
+
+server.listen( PORT, () => console.log('Listening on port', PORT));
+
